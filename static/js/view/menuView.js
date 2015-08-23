d461b22a191e100372090676ca4a981bdbf8deab
Alberto Pedron <pedron.albert@gmail.com>
2015-08-23 18:50:09 -0430

    testing typehead

--- static/js/view/menuView.js
+++ static/js/view/menuView.js
@@ -21,16 +21,8 @@ module.exports = Backbone.View.extend({
           return function findMatches(q, cb) {
           var matches, substringRegex;
           // an array that will be populated with substring matches
-          matches = [];
-          // regex used to determine if a string contains the substring `q`
-          substrRegex = new RegExp(q, 'i');
-          // iterate through the pool of strings and for any string that
-          // contains the substring `q`, add it to the `matches` array
-          $.each(strs, function(i, str) {
-            if (substrRegex.test(str)) {
-                matches.push(str);
-            }
-          });
+          matches = ['hola', 'mundo'];
+
           cb(matches);
           };
       };
