7751b37b735de94ee54f73b0946ec32def7fa0e1
wilmer <mvc_wil@outlook.es>
2015-09-27 23:15:19 -0430

    action today div error

--- static/js/view/action/actionTodayTableView.js
+++ static/js/view/action/actionTodayTableView.js
@@ -16,16 +16,20 @@ module.exports = Backbone.View.extend({
 
   render: function () {
     $.get(rootView + this.template, function (template) {
-      if (_.isEmpty(this.message)) {
-        var template = template;
+      var template = Handlebars.compile(template);
+      var erroMessage = {message: this.message};
+      var html = template(erroMessage);
+      
+      this.$el.html(html);
 
-        this.$el.html(template);
-        this.$tbody = this.$el.find('table').children('tbody');
+      if (_.isEmpty(this.message)) {
+        this.$tbody = this
+                        .$el
+                        .find('table')
+                        .children('tbody');
 
         this.addAll();
-      } else {
-        this.emptyAction(this.message);
-      }
+      } 
     }.bind(this))
     
   },
@@ -44,13 +48,6 @@ module.exports = Backbone.View.extend({
    var actionToday = new ActionToday({model: action});
 
     this.$tbody.append(actionToday.render().el);
-  },
-
-  emptyAction: function (message) {
-    var erroMessage = {message: message};
-    var boxError = this.boxError(erroMessage);
-
-    this.$el.html(boxError);
   },
 
   close: function () {
